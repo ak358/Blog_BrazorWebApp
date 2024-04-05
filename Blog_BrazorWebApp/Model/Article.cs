06af40053a59e187a8c3b7ea324ff0a28cb10ee7
ak358 <arikana.358code@gmail.com>
2024-04-06 03:23:59 +0900

    Articleモデルを追加しました

---
+++ Blog_BrazorWebApp/Model/Article.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog_BrazorWebApp.Model
+{
+    public class Article
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(255)")]
+        public string Title { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(max)")]
+        public string Content { get; set; } = "";
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime UpdateDate { get; set; }
+
+    }
+}
