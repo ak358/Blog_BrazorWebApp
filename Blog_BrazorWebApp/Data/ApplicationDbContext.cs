df848c697b48683e14ec3f4a9bff4dcc5696972d
ak358 <arikana.358code@gmail.com>
2024-04-06 03:27:46 +0900

    ApplicationDbContextクラスを追加しました

---
+++ Blog_BrazorWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Blog_BrazorWebApp.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog_BrazorWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Article>().HasData(
+                new Article { Id = 1, Title = "タイトル１", Content = "本文１", CreateDate = DateTime.Now, UpdateDate = DateTime.Now },
+                new Article { Id = 2, Title = "タイトル２", Content = "本文２", CreateDate = DateTime.Now, UpdateDate = DateTime.Now },
+                new Article { Id = 3, Title = "タイトル３", Content = "本文３", CreateDate = DateTime.Now, UpdateDate = DateTime.Now }
+            );
+        }
+    }
+}
